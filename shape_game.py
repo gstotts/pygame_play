4242d00097061349b0e7814a07941377bf9160ca
Greg <mr.stotts@gmail.com>
2018-11-20 19:22:39 -0600

    Added Shapes to Initial Board based on Board Array

--- shape_game.py
+++ shape_game.py
@@ -4,26 +4,23 @@ import random, pygame, sys
 from core import colors
 
 FPS = 30
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
+WINDOW_WIDTH = 510
+WINDOW_HEIGHT = 510
 BOXSIZE = 40
 GAPSIZE = 10
 BOARD_WIDTH = 10
-BOARD_HEIGHT = 7
-X_MARGIN = int((WINDOW_WIDTH - (BOARD_WIDTH * (BOXSIZE + GAPSIZE))) / 2)
-Y_MARGIN = int((WINDOW_HEIGHT - (BOARD_HEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+BOARD_HEIGHT = 10
 
-BGCOLOR = colors.NAVY
-LIGHT_BGCOLOR = colors.GRAY
-HIGHLIGHT = colors.BLUE
+BGCOLOR = colors.GRAY
 
 # Shapes
 SQUARE = 0
 DIAMOND = 1
-OVAL = 2
+CIRCLE = 2
 TRIANGLE = 3
 
-ALL_SHAPES = [SQUARE, DIAMOND, OVAL, TRIANGLE]
+ALL_SHAPES = [SQUARE, DIAMOND, CIRCLE, TRIANGLE]
+
 
 class ShapeGame:
     def __init__(self):
@@ -46,7 +43,30 @@ class ShapeGame:
         return board
 
     def draw_board(self, board):
-        print(board)
+        y = GAPSIZE
+        self.screen.fill(BGCOLOR)
+        for row in board:
+            x = GAPSIZE
+            for item in row:
+                print (x, y, x + BOXSIZE, y + BOXSIZE)
+                if item == SQUARE:
+                    pygame.draw.rect(self.screen, colors.GREEN, (x, y, BOXSIZE, BOXSIZE))
+                elif item == DIAMOND:
+                    pygame.draw.polygon(self.screen, colors.BLUE, ((x + int(BOXSIZE * .5), y), \
+                                                                   (x + BOXSIZE, y + int(BOXSIZE * .5)), \
+                                                                   (x + int(BOXSIZE * .5), y + BOXSIZE), \
+                                                                   (x, y + int(BOXSIZE * .5))))
+                elif item == CIRCLE:
+                    pygame.draw.circle(self.screen, colors.RED, (x + int(BOXSIZE / 2), \
+                                                                 y + int(BOXSIZE / 2)), int(BOXSIZE / 2))
+                else:
+                    pygame.draw.polygon(self.screen, colors.YELLOW, ((x + int(BOXSIZE * .5), y), \
+                                                                     (x + BOXSIZE, y + BOXSIZE), \
+                                                                     (x, y + BOXSIZE)))
+                x += (BOXSIZE + GAPSIZE)
+            y += (BOXSIZE + GAPSIZE)
+
+        pygame.display.update()
 
     def run(self):
         x, y = (0, 0)
@@ -57,6 +77,7 @@ class ShapeGame:
                 if event.type == pygame.QUIT:
                     self.running = False
                     pygame.quit()
+
 
 if __name__ == '__main__':
     ShapeGame().run()
